f72e3c0fc5a470aa5d8ca8eb60c89bdd28af94f0
JBfreezing <1774082746@qq.com>
2025-06-20 20:33:46 +0800

    后端项目创建

---
+++ src/main/java/com/itbin/gamerssystem/GamersSystemApplication.java
@@ -0,0 +1,13 @@
+package com.itbin.gamerssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamersSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamersSystemApplication.class, args);
+    }
+
+}
